ab8c867f24b4506cb43a7bd4826ef3ea11e9ec50
Gabriel Costa <gabrielcostamgm@hotmail.com>
2025-11-22 16:29:56 -0300

    feat(ArvoreBinaria): adiciona métodos de geração de valores crescente, decrescente e aleatório

--- ArvoreDeBuscaBinaria/ArvoreBinaria.java
+++ ArvoreDeBuscaBinaria/ArvoreBinaria.java
@@ -1,5 +1,7 @@
 package  ArvoreDeBuscaBinaria;
 
+import java.util.Random;
+
 public class ArvoreBinaria {
     private class No {
         int valor;
@@ -26,25 +28,36 @@ public class ArvoreBinaria {
         } else if (valor > atual.valor) {
             atual.direita = inserirRecursivo(atual.direita, valor);
         }
-        return atual;
+        return
+         atual;
     }
-    public boolean buscar(int valor) {
-        return buscarRecursivo(raiz, valor);
+    public static int[] gerarValoresCresc(int tamanho) {
+    int[] vetorGerado = new int[tamanho];
+    for (int i = 0; i < tamanho; i++) {
+        vetorGerado[i] = i + 1;
+    }
+    return vetorGerado;
+}
+
+public static int[] gerarValoresDesc(int tamanho) {
+    int[] vetorGerado = new int[tamanho];
+    for (int i = 0; i < tamanho; i++) {
+        vetorGerado[i] = tamanho - i;
+    }
+    return vetorGerado;
+}
+
+public static int[] gerarValoresRandom(int tamanho) {
+    int[] vetorGerado = gerarValoresCresc(tamanho);
+    Random random = new Random();
+    for (int i = tamanho - 1; i > 0; i--) {
+        int j = random.nextInt(i + 1);
+        int temp = vetorGerado[i];
+        vetorGerado[i] = vetorGerado[j];
+        vetorGerado[j] = temp;
     }
 
-    private boolean buscarRecursivo(No atual, int valor) {
-        if (atual == null) {
-            return false;
-        }
-        if (valor == atual.valor) {
-            return true;
-        }
-        if (valor < atual.valor) {
-            return buscarRecursivo(atual.esquerda, valor);
-        } else {
-            return buscarRecursivo(atual.direita, valor);
-        }
-    }
-
+    return vetorGerado;
+}
 } 
     
