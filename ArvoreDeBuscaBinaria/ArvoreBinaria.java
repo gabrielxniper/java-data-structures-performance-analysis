4b074128f24b7bf72dcce5d41946403f8b2797d8
Gabriel Costa <gabrielcostamgm@hotmail.com>
2025-11-22 16:39:42 -0300

    feat(ArvoreBinaria): adiciona método de busca recursiva

--- ArvoreDeBuscaBinaria/ArvoreBinaria.java
+++ ArvoreDeBuscaBinaria/ArvoreBinaria.java
@@ -31,33 +31,51 @@ public class ArvoreBinaria {
         return
          atual;
     }
+    public boolean buscar(int valor) {
+        return buscarRecursivo(raiz, valor);
+    }
+    
+    private boolean buscarRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return false;
+        }
+        if (valor == atual.valor) {
+            return true;
+        }
+        if (valor < atual.valor) {
+            return buscarRecursivo(atual.esquerda, valor);
+        } else {
+            return buscarRecursivo(atual.direita, valor);
+        }
+    }
+
     public static int[] gerarValoresCresc(int tamanho) {
-    int[] vetorGerado = new int[tamanho];
-    for (int i = 0; i < tamanho; i++) {
-        vetorGerado[i] = i + 1;
-    }
-    return vetorGerado;
-}
-
-public static int[] gerarValoresDesc(int tamanho) {
-    int[] vetorGerado = new int[tamanho];
-    for (int i = 0; i < tamanho; i++) {
-        vetorGerado[i] = tamanho - i;
-    }
-    return vetorGerado;
-}
-
-public static int[] gerarValoresRandom(int tamanho) {
-    int[] vetorGerado = gerarValoresCresc(tamanho);
-    Random random = new Random();
-    for (int i = tamanho - 1; i > 0; i--) {
-        int j = random.nextInt(i + 1);
-        int temp = vetorGerado[i];
-        vetorGerado[i] = vetorGerado[j];
-        vetorGerado[j] = temp;
+        int[] vetorGerado = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetorGerado[i] = i + 1;
+        }
+        return vetorGerado;
     }
 
-    return vetorGerado;
-}
+    public static int[] gerarValoresDesc(int tamanho) {
+        int[] vetorGerado = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetorGerado[i] = tamanho - i;
+        }
+        return vetorGerado;
+    }
+
+    public static int[] gerarValoresRandom(int tamanho) {
+        int[] vetorGerado = gerarValoresCresc(tamanho);
+        Random random = new Random();
+        for (int i = tamanho - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = vetorGerado[i];
+            vetorGerado[i] = vetorGerado[j];
+            vetorGerado[j] = temp;
+        }
+
+        return vetorGerado;
+    }
 } 
     
